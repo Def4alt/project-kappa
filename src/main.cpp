f1eead24eba3d14a6f2cfd8b9eb73017f43e02de
Andrii Olkhovych <bronzemonet@gmail.com>
2020-10-14 20:09:01 +0300

    reboot

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,71 @@
-#include <SDL.h>
-#include <imgui/imgui.h>
-#include "logger.h"
-#include "game.h"
+#include <functional>
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+#include <docopt/docopt.h>
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 
 #undef main
 
-int main() {
-    engine::Logger::init();
+#define IMGUI_IMPL_OPENGL_LOADER_GLEW
 
+static constexpr auto USAGE =
+    R"(Project Kappa.
 
-    auto *game =
-            new Game("Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                    800, 600, false);
+    Usage:
+        project_kappa [options]
 
-    while (game->is_running) {
-        game->handle_events();
+ 	Options:
+        -h --help         Print help.
+        --width=WIDTH     Width of the main window [default: 800].
+        --height=HEIGHT   Height of the main window [default: 600].
+)";
 
-        game->update(1000 / ImGui::GetIO().Framerate);
-        game->render();
-        game->imgui_render();
-        
-        game->swap_window();
+int main(int argc, const char **argv) {
+
+  std::map<std::string, docopt::value> args =
+      docopt::docopt(USAGE, {std::next(argv), std::next(argv, argc)}, true,
+                     "Project Kappa 1.0");
+
+  for (auto const &arg : args) {
+    if (arg.second.isString()) {
+      spdlog::info("Parameter set: {} '{}'", arg.first, arg.second.asString());
     }
+  }
 
-    delete game;
+  const auto width = args["--width"].asLong();
+  const auto height = args["--height"].asLong();
 
-    return 0;
+  SDL_Window *window = NULL;
+  SDL_Surface *screenSurface = NULL;
+
+  window = SDL_CreateWindow("Project Kappa", SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, width, height,
+                            SDL_WINDOW_SHOWN);
+
+  if (window == NULL) {
+    spdlog::error("Could not create window: {}", SDL_GetError());
+    abort();
+  }
+
+  screenSurface = SDL_GetWindowSurface(window);
+
+  SDL_FillRect(screenSurface, NULL,
+               SDL_MapRGB(screenSurface->format, 0x00, 0xFF, 0xFF));
+
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(2000);
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
